d44184d18e915f975476d59f9b89b9fb028dc96b
DouglasAntunes <douglasblackburn007@gmail.com>
2019-06-24 20:17:14 -0300

    Fixed & Added more tests to SteamWorkshopServiceTest class. -Fixed the url "FailsToFetchCollectionFromInvalidUrlAndThrowAnException" & added specific exception detection -Added more tests

--- KF2 Workshop URL Converter/KF2WorkshopUrlConverter.Test/Services/SteamWorkshopServiceTest.cs
+++ KF2 Workshop URL Converter/KF2WorkshopUrlConverter.Test/Services/SteamWorkshopServiceTest.cs
@@ -1,5 +1,7 @@
+using KF2WorkshopUrlConverter.Core.SteamWorkshop.Exceptions;
 using KF2WorkshopUrlConverter.Core.SteamWorkshop.Services;
 using NUnit.Framework;
+using System;
 
 namespace KF2WorkshopUrlConverter.Test.Services
 {
@@ -11,8 +13,41 @@ namespace KF2WorkshopUrlConverter.Test.Services
         [Test]
         public void FailsToFetchCollectionFromInvalidUrlAndThrowAnException()
         {
-            var testUrl = "https://www.google.com.br";
-            Assert.That(() => workshopService.FetchCollectionFromURL(testUrl), Throws.Exception);
+            var testUrl = "randomTextAndNotAUrl";
+            Assert.That(() => workshopService.FetchCollectionFromURL(testUrl), Throws.TypeOf<NotASteamWorkshopUrlException>());
         }
+
+        [Test]
+        public void FailsToFetchCollectionFromValidUrlButNotASteamWorkshopUrlAndThrowAnException()
+        {
+            var testUrl = "https://google.com.br";
+            Assert.That(() => workshopService.FetchCollectionFromURL(testUrl), Throws.TypeOf<NotASteamWorkshopUrlException>());
+        }
+
+        [Test]
+        public void FailsToFetchCollectionFromValidSteamWorkshopUrlWithoutProtocolAndThrowAnException()
+        {
+            var testUrl = "steamcommunity.com/sharedfiles/filedetails/?id=882417829";
+            Assert.That(() => workshopService.FetchCollectionFromURL(testUrl), Throws.TypeOf<UriFormatException>());
+        }
+
+        [Test]
+        public void FailsToFetchCollectionFromASteamWorkshopItemUrlAndThrowAnException()
+        {
+            var testUrl = "https://steamcommunity.com/sharedfiles/filedetails/?id=650252240";
+            Assert.That(() => workshopService.FetchCollectionFromURL(testUrl), Throws.TypeOf<NotACollectionException>());
+        }
+
+        [Test]
+        public void SuccededToFetchTheProjectExampleCollection()
+        {
+            var testUrl = "https://steamcommunity.com/sharedfiles/filedetails/?id=882417829";
+            var collection = workshopService.FetchCollectionFromURL(testUrl);
+            Assert.IsTrue(collection.ItemCount == 1);
+            Assert.IsTrue(collection.Name.Equals("Map Collection Example"));
+            Assert.IsTrue(collection.Items[0].Url.Equals("https://steamcommunity.com/sharedfiles/filedetails/?id=650252240"));
+        }
+
+
     }
 }
