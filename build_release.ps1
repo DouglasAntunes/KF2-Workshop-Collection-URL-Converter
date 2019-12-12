61a8667af600875eb3990a89cc489c29cf187ba3
Douglas Antunes <DouglasAntunes@users.noreply.github.com>
2019-12-11 21:30:19 -0300

    .Net Core 3.1 Update (#5)
    * Updated Project to .Net Core 3.1
    * Added Script to Automate Builds for supported plataforms.
    * Updated README.

---
+++ build_release.ps1
@@ -0,0 +1,58 @@
+﻿function clean {
+    param([string] $ProjectFolder)
+    Remove-Item -Path $ProjectFolder\bin\ -Recurse
+    Remove-Item -Path $ProjectFolder\obj\ -Recurse
+}
+
+function dotnetPublish {
+    param([string]$Project, [string]$Runtime)
+    dotnet publish $Project -c Release -f "netcoreapp3.1" --self-contained true -r $Runtime -v q /p:PublishSingleFile=true /p:PublishTrimmed=true
+}
+
+function compressAndRemove {
+    param([string] $File, [string] $ZipName)
+    Compress-Archive -Path $File -DestinationPath $ZipName
+    Remove-Item -Path $File
+}
+
+
+#MAIN
+
+$version = Read-Host "Please enter the version string (ex: 1.0)"
+
+$projectPath = Get-Item -Path '.\KF2 Workshop URL Converter\KF2WorkshopUrlConverter.Core'
+$releaseFolderPath = Get-Item -Path .\releaseZips
+
+if(($releaseFolderPath).Exists) {
+    Write-Host 'The Release folder named "'($releaseFolderPath).Name '" already exists, removing files inside folder...' -Separator '' -ForegroundColor Red
+    Remove-Item $releaseFolderPath\*.*
+} else {
+    Write-Host 'The Release folder named "' ($releaseFolderPath).Name '" not exists, creating folder...' -Separator '' -ForegroundColor DarkRed
+    New-Item -Path .\releaseZips\ -ItemType Directory
+}
+
+Write-Host "Building win-x64..." -ForegroundColor DarkGreen
+
+clean -ProjectFolder $projectPath
+dotnetPublish -Project $projectPath -Runtime "win-x64"
+
+Move-Item -Path $projectPath\bin\Release\netcoreapp3.1\win-x64\publish\KF2WorkshopUrlConverter.exe -Destination $releaseFolderPath
+compressAndRemove -File $releaseFolderPath\KF2WorkshopUrlConverter.exe -ZipName $releaseFolderPath\KF2WorkshopUrlConverter.v$version-win64.zip
+
+Write-Host "Building linux-x64..." -ForegroundColor DarkGreen
+
+clean -ProjectFolder $projectPath
+dotnetPublish -Project $projectPath -Runtime "linux-x64"
+
+Move-Item -Path $projectPath\bin\Release\netcoreapp3.1\linux-x64\publish\KF2WorkshopUrlConverter -Destination $releaseFolderPath
+compressAndRemove -File $releaseFolderPath\KF2WorkshopUrlConverter -ZipName $releaseFolderPath\KF2WorkshopUrlConverter.v$version-linux64.zip
+
+Write-Host "Building osx-x64..." -ForegroundColor DarkGreen
+
+clean -ProjectFolder $projectPath
+dotnetPublish -Project $projectPath -Runtime "osx-x64"
+
+Move-Item -Path $projectPath\bin\Release\netcoreapp3.1\osx-x64\publish\KF2WorkshopUrlConverter -Destination $releaseFolderPath
+compressAndRemove -File $releaseFolderPath\KF2WorkshopUrlConverter -ZipName $releaseFolderPath\KF2WorkshopUrlConverter.v$version-macos64.zip
+
+Write-Host "Done" -ForegroundColor Green
