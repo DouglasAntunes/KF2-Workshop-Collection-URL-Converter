3247d24bfca951428b1112d4fe7a7978f5605e1b
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2021-01-04 17:14:22 -0300

    test: Dummy test for ProgramStrings autogenerated class.

---
+++ KF2 Workshop URL Converter/KF2WorkshopUrlConverter.Test/ProgramStringsTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+using KF2WorkshopUrlConverter.Core.Properties;
+
+namespace KF2WorkshopUrlConverter.Test
+{
+    [TestFixture]
+    class ProgramStringsTest
+    {
+        [Test, Description("Dummy Test the default constructor")]
+        public void ConstructorEmpty() {
+            ProgramStrings programStrings = new ProgramStrings();
+            Assert.IsNotNull(programStrings);
+        }
+
+        [Test, Description("Dummy test to cover get/set of the property Culture")]
+        public void CultureInfoData()
+        {
+            string cultureName = "en-US";
+            CultureInfo toApply = new CultureInfo(cultureName);
+
+            ProgramStrings.Culture = toApply;
+            CultureInfo culture = ProgramStrings.Culture;
+
+            Assert.IsNotNull(culture);
+            Assert.AreEqual(cultureName, culture.Name);
+        }
+    }
+}
